c4a42ba5bdc1bd46e930d386698f2e933d7e2dac
HTMLProgrammer2001 <cssuperpy@gmail.com>
2020-03-21 14:12:51 +0200

    Before selection logic change

---
+++ dist/php/FileSystem.php
@@ -0,0 +1,113 @@
+<?php
+
+
+class FileSystem
+{
+    private $path;
+    private $isDir;
+
+    //excluding files
+    protected $exclude = ['.', '..'];
+
+    public function __construct($path = './'){
+        $this->path = $path;
+        $this->isDir = is_dir($path);
+    }
+
+    //return array of content of dir
+    public function getDirContent(){
+        $files = [
+            'errors' => '',
+            'path' => $this->path,
+            'value' => []
+        ];
+
+        //if it's not a dir then return error
+        if(!$this->isDir) {
+            $files['errors'] = 'Only dir may include files';
+            return $files;
+        }
+
+        $dir = opendir($this->path);
+
+        //read content of dir
+        while($file = readdir($dir)){
+            if(in_array($file, $this->exclude))
+                continue;
+
+            $files['value'][] = $this->getFileInfo($file, ($this->path).$file);
+        }
+
+        return $files;
+    }
+
+    //return hash table with info about dir item
+    protected function getFileInfo($name, $path){
+        $fileInfo = [
+            'name' => $name,
+            'path' => $path
+        ];
+
+        //return dir info
+        if(is_dir($path)){
+            $fileInfo['isDir'] = true;
+
+            return $fileInfo;
+        }
+
+        //get type
+        $fileInfo['type'] = substr($name, strrpos($name, '.') + 1);
+
+        return $fileInfo;
+    }
+
+    public function getFileContent(){
+        return file_get_contents($this->path);
+    }
+
+    public static function delete($paths){
+        foreach ($paths as $path){
+            if(!file_exists($path)) {
+                echo "$path not exist";
+                continue;
+            }
+
+            if(is_dir($path))
+                rmdir($path);
+            else
+                unlink($path);
+        }
+    }
+
+    public static function create($path, $type){
+        if($type == 'file')
+            fopen($path, 'w');
+        else
+            mkdir($path);
+    }
+
+    public static function rename($from, $to){
+        if(!file_exists($from)) {
+            echo "$from not exist";
+            return;
+        }
+
+        rename($from, $to);
+    }
+    public static function move($paths, $to){
+        foreach ($paths as $path){
+            if(!file_exists($path)){
+                echo "$path not exist";
+                continue;
+            }
+
+            rename($path, $to . '/' . basename($path));
+        }
+    }
+
+    public static function save($path, $content){
+        $file = fopen($path, 'w');
+        fwrite($file, $content);
+        fclose($file);
+    }
+}
---
+++ dist/php/api.php
@@ -0,0 +1,42 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: PUT, GET, PATCH, POST, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Origin');
+header('Access-Control-Expose-Headers: Content-Type, Origin');
+
+//include filesystem class
+require('./FileSystem.php');
+
+if($_POST['type'] === 'getFolderContent'){
+    $path = './';
+
+    if (isset($_POST['path']))
+        $path = $_POST['path'];
+
+    $fileSystem = new FileSystem($path);
+    $content = $fileSystem->getDirContent();
+    echo json_encode($content);
+}
+
+if($_POST['type'] === 'getFileContent'){
+    $fileSystem = new FileSystem($_POST['path']);
+
+    echo $fileSystem->getFileContent();
+}
+
+if($_POST['type'] === 'delete')
+    FileSystem::delete(json_decode($_POST['paths']));
+
+if($_POST['type'] === 'create') {
+    FileSystem::create($_POST['path'], $_POST['createType']);
+}
+
+if($_POST['type'] === 'rename')
+    FileSystem::rename($_POST['from'], $_POST['to']);
+
+if($_POST['type'] === 'move')
+    FileSystem::move(json_decode($_POST['paths']), $_POST['to']);
+
+if($_POST['type'] === 'saveFileContent')
+    FileSystem::save($_POST['path'], $_POST['content']);
